87a4ea2c9abc0ca0ae742615143f93863039878a
Jake Zimmerman <zimmerman.jake@gmail.com>
2018-10-13 22:41:04 -0700

    Add with=mlton tests

---
+++ tests/symbol/with-mlton.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+source tests/logging.sh
+
+cd scaffold
+
+symbol make with=mlton
+
+target=.symbol-work/bin/TARGET
+
+if ! [ -f "$target" ]; then
+  fatal "Didn't build successfully."
+fi
+
+if ! [ -x "$target" ]; then
+  fatal "TARGET is not executable."
+fi
+
+is_binary() {
+  ! grep -qI '.' "$1"
+}
+
+if ! is_binary "$target"; then
+  fatal "TARGET is not a binary; are we sure it was built with MLton?"
+fi
+
+if ! "$target"; then
+  fatal "Running TARGET returned non-zero status"
+fi
